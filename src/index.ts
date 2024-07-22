4b71a03cf348f352533210205d021999aa068cd6
Alexandre Castlenine <alexandre@duxreserve.com>
2024-07-22 17:02:15 +0200

    feat: individual function importing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Import all utility functions
 import * as atomicUnit from './utils/atomic-unit';
 import * as cookie from './utils/cookie';
 import * as cryptocurrency from './utils/cryptocurrency';
@@ -9,6 +10,7 @@ import * as object from './utils/object';
 import * as string from './utils/string';
 import * as time from './utils/time';
 
+// Create the utilityBelt object
 const utilityBelt = {
 	...atomicUnit,
 	...cookie,
@@ -22,7 +24,69 @@ const utilityBelt = {
 	...time,
 };
 
+// Default export
 export default utilityBelt;
 
 // Named exports for individual modules
 export { atomicUnit, cookie, cryptocurrency, currency, delay, email, number, object, string, time };
+
+// Named exports for individual functions
+export { convertNumberToAtomicUnit, convertAtomicUnitToBigNumber } from './utils/atomic-unit';
+
+export { parseValueFromCookie } from './utils/cookie';
+
+export { cryptoMinusShiftedToBigNumberType, cryptoShiftedToBigNumberType } from './utils/cryptocurrency';
+
+export { getCurrencyFullName, getCurrencySymbol, formatAndAddCurrencySymbol, labelCurrency } from './utils/currency';
+
+export { isEmailValid } from './utils/email';
+
+export {
+	isNumber,
+	stringToBigNumber,
+	absoluteToBigNumber,
+	truncateNumberToBigNumber,
+	roundNumberToBigNumber,
+	roundUpNumberToBigNumber,
+	roundDownNumberToBigNumber,
+	formatNumber,
+	countDecimalPlaces,
+	labelNumber,
+} from './utils/number';
+
+export { isObjectEmpty, countValueInArrayOfObjects } from './utils/object';
+
+export {
+	capitalizeFirstLetterOnly,
+	replaceLastCommaByDot,
+	isStringContainsNumber,
+	removeNonNumericCharactersFromString,
+	removeNumbersFromString,
+	numberToString,
+	normalizeString,
+	slugifyString,
+} from './utils/string';
+
+export {
+	getStringDifferenceFromNow,
+	getStringDifferenceToNow,
+	getStringDifferenceFromDateTime,
+	getStringDifferenceToDateTime,
+	isDateBetween,
+	isDateSame,
+	isDateSameOrBefore,
+	isDateSameOrAfter,
+	isDateYesterday,
+	isDateToday,
+	isDateTomorrow,
+	isDateLeapYear,
+	howManyLeapDayInLastYears,
+	daysInCurrentYear,
+	daysSinceStartOfCurrentYear,
+	guessUserTimezone,
+	changeTimezone,
+	currentUtcTime,
+	isDateUtc,
+	convertDateToUTC,
+	convertDateToLocalTime,
+} from './utils/time';
